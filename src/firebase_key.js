8d98fa2c1ee18acc8b33a20c118888c5998c4790
Reyzartz <reyanrahman@gmail.com>
2020-11-29 11:29:40 +0530

    deploying website

---
+++ src/firebase_key.js
@@ -0,0 +1,10 @@
+const Firebase_key={
+    apiKey: "AIzaSyA9n5mH6Db25stWGoILypbswnzEe95CXoo",
+    authDomain: "react-messaging-application.firebaseapp.com",
+    databaseURL: "https://react-messaging-application.firebaseio.com",
+    projectId: "react-messaging-application",
+    storageBucket: "react-messaging-application.appspot.com",
+    messagingSenderId: "300406475041",
+    appId: "1:300406475041:web:53b65da2127df4a2"
+}
+export default Firebase_key;
